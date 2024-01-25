0f507cd95790135236c8a59fd7039f412a10b4ea
Cocoa <i@uwucocoa.moe>
2024-01-25 15:32:52 +0700

    [build] build full image

--- .github/scripts/stage3.sh
+++ .github/scripts/stage3.sh
@@ -11,17 +11,17 @@ git checkout "$GIT_COMMIT"
 
 if [ "${ARCHITECTURE}" = "hybrid" ]; then
     python3 ./cheribuild.py --enable-hybrid-targets "cheribsd-sdk-morello-${ARCHITECTURE}" -f 
-    python3 ./cheribuild.py --enable-hybrid-targets "disk-image-minimal-morello-${ARCHITECTURE}" -f
+    python3 ./cheribuild.py --enable-hybrid-targets "disk-image-morello-${ARCHITECTURE}" -f
 else
     python3 ./cheribuild.py "cheribsd-sdk-morello-${ARCHITECTURE}" -f 
-    python3 ./cheribuild.py "disk-image-minimal-morello-${ARCHITECTURE}" -f
+    python3 ./cheribuild.py "disk-image-morello-${ARCHITECTURE}" -f
 fi
 
 export XZ_OPT="-e -T0 -9"
 
-xz -z -e -T0 -9 "${HOME}/cheri/output/cheribsd-minimal-morello-${ARCHITECTURE}.img"
-rm -f "${HOME}/cheri/output/cheribsd-minimal-morello-${ARCHITECTURE}.img"
-mv "${HOME}/cheri/output/cheribsd-minimal-morello-${ARCHITECTURE}.img.xz" /work/build/
+xz -z -e -T0 -9 "${HOME}/cheri/output/cheribsd-morello-${ARCHITECTURE}.img"
+rm -f "${HOME}/cheri/output/cheribsd-morello-${ARCHITECTURE}.img"
+mv "${HOME}/cheri/output/cheribsd-morello-${ARCHITECTURE}.img.xz" /work/build/
 
 tar -C "${HOME}/cheri/output" -cJf "$(pwd)/sdk-${ARCHITECTURE}.tar.xz" sdk
 mv "$(pwd)/sdk-${ARCHITECTURE}.tar.xz" /work/build/
@@ -30,7 +30,7 @@ tar -C "${HOME}/cheri/output" -cJf "$(pwd)/morello-sdk-${ARCHITECTURE}.tar.xz" m
 mv "$(pwd)/morello-sdk-${ARCHITECTURE}.tar.xz" /work/build/
 
 cd /work/build
-sha256sum cheribsd-minimal-morello-${ARCHITECTURE}.img.xz | tee cheribsd-minimal-morello-${ARCHITECTURE}.img.xz.sha256
+sha256sum cheribsd-morello-${ARCHITECTURE}.img.xz | tee cheribsd-morello-${ARCHITECTURE}.img.xz.sha256
 sha256sum sdk-${ARCHITECTURE}.tar.xz | tee sdk-${ARCHITECTURE}.tar.xz.sha256
 sha256sum morello-sdk-${ARCHITECTURE}.tar.xz | tee morello-sdk-${ARCHITECTURE}.tar.xz.sha256
 
