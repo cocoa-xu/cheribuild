9d4d8c96cb9e3117bd5300319e8d308e7d9da684
Cocoa <i@uwucocoa.moe>
2024-01-26 19:29:07 +0700

    [ci-linux] ensure it runs on correct platform

--- .github/scripts/stage1.sh
+++ .github/scripts/stage1.sh
@@ -7,5 +7,5 @@ ARCHITECTURE=$2
 TARGETS=$3
 HOST_TRIPLET=$4
 
-sudo docker run --privileged --network=host --rm -v $(pwd):/work "ubuntu:latest" \
+sudo docker run --privileged --network=host --rm --platform="${DOCKER_PLATFORM}" -v $(pwd):/work "ubuntu:latest" \
     sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${GIT_COMMIT} ${ARCHITECTURE} ${TARGETS} ${HOST_TRIPLET}"
