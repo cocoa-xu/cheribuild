9c866e1419f1025d37a40fe514132f38652c323e
Cocoa <i@uwucocoa.moe>
2024-01-26 09:20:15 +0700

    [ci-aarch64-linux-gnu] runs on self-hosted-x86_64-linux-gnu with --platform=linux/arm64

--- .github/scripts/stage1.sh
+++ .github/scripts/stage1.sh
@@ -7,5 +7,10 @@ ARCHITECTURE=$2
 TARGETS=$3
 HOST_TRIPLET=$4
 
-sudo docker run --privileged --network=host --rm -v $(pwd):/work "ubuntu:latest" \
-    sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${GIT_COMMIT} ${ARCHITECTURE} ${TARGETS} ${HOST_TRIPLET}"
+if [ "${DOCKER_PLATFORM}" = "linux/arm64" ]; then
+    sudo docker run --privileged --network=host --rm --platform="linux/arm64" -v $(pwd):/work "ubuntu:latest" \
+        sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${GIT_COMMIT} ${ARCHITECTURE} ${TARGETS} ${HOST_TRIPLET}"
+else
+    sudo docker run --privileged --network=host --rm -v $(pwd):/work "ubuntu:latest" \
+        sh -c "chmod a+x /work/stage2.sh && /work/stage2.sh ${GIT_COMMIT} ${ARCHITECTURE} ${TARGETS} ${HOST_TRIPLET}"
+fi
