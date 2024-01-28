3e9e6c42e9fe1915ba32a70234e162adac888ca6
Cocoa <i@uwucocoa.moe>
2024-01-28 20:33:52 +0700

    [docker] added local docker buildx script

--- .github/docker/buildx.sh
+++ .github/docker/buildx.sh
@@ -11,23 +11,21 @@ export PLATFORM="linux/amd64,linux/arm64"
 
 export ARCHIECTURE=purecap
 export DISK_IMAGE=normal
-docker buildx build --platform "${PLATFORM}" . \
+docker buildx build --push --platform "${PLATFORM}" . \
     -t cocoaxu/cheribsd-"${ARCHIECTURE}":latest \
     -t cocoaxu/cheribsd-"${ARCHIECTURE}":"${VERSION}" \
     --build-arg architecture="${ARCHIECTURE}" \
     --build-arg version="${VERSION}" \
-    --build-arg disk_image="${DISK_IMAGE}" \
-    --push
+    --build-arg disk_image="${DISK_IMAGE}"
 
 export ARCHIECTURE=purecap
 export DISK_IMAGE=minimal
-docker buildx build --platform "${PLATFORM}" . \
+docker buildx build --push --platform "${PLATFORM}" . \
     -t cocoaxu/cheribsd-minimal-"${ARCHIECTURE}":latest \
     -t cocoaxu/cheribsd-minimal-"${ARCHIECTURE}":"${VERSION}" \
     --build-arg architecture="${ARCHIECTURE}" \
     --build-arg version="${VERSION}" \
-    --build-arg disk_image="${DISK_IMAGE}" \
-    --push
+    --build-arg disk_image="${DISK_IMAGE}"
 
 export ARCHIECTURE=hybrid
 export DISK_IMAGE=normal
